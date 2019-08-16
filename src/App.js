ee5bda0985cf68c995568d4d779dd1d1ce8b859e
airportmarc <marcwitney@live.com>
2019-08-16 06:52:39 -0400

    Set the env for the app to allow for different env

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import {ButtonBody} from "./Components/ButtonBody";
 function App() {
   return (
     <div>
-      <a href="https://accounts.spotify.com/authorize?client_id=36421c22177a461097ba41df86d40ec5&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fredirect&scope=user-read-private%20user-read-email&response_type=token&state=123">
+      <a
+        href={`https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_SPOTIFY_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_SPOTIFY_REDIRECT_URI}&scope=user-read-private%20user-read-email&response_type=token&state=123`}>
         Login with Spotify
       </a>
       <h1 className="pt-24">Welcome, Its Friday afternoon, what are you in the mood for?</h1>
