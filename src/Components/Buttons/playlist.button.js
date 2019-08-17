650a7e01b158f90ce5153beffc8ee6a172cad611
airportmarc <marcwitney@live.com>
2019-08-17 11:13:07 -0400

    add polish to the playlist button

--- src/Components/Buttons/playlist.button.js
+++ src/Components/Buttons/playlist.button.js
@@ -2,10 +2,18 @@ import React from 'react'
 
 
 export function PlaylistButton(props){
-  const {handleClick, item: {name, images: [image], id} } = props
+  const {handleClick, item: {name, images: [image], id, external_urls} } = props
     return (
-      <div className="flex flex-col mx-4 my-4 relative" onClick={() => handleClick(id)}>
+      <div className="flex flex-col mx-4 my-4 relative" >
         <img src={image.url} width={image.width || 275} height={image.height || 275}/>
+        <div className="opacity-0 hover:bg-gray-700 hover:opacity-75 absolute bottom-0 flex text-white justify-between items-center w-full text-center h-full">
+            <div className="w-1/2 border-r-2 cursor-pointer" onClick={() => handleClick(id)}>
+            Tracks
+          </div>
+          <div className="w-1/2">
+            <a href={external_urls['spotify']}>Open</a>
+          </div>
+        </div>
       </div>)
 
 
