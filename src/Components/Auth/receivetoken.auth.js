7d591f455e3a93f3a8fb75cb1b1beff3df27f53c
airportmarc <marcwitney@live.com>
2019-08-17 11:46:52 -0400

    adding some comments

--- src/Components/Auth/receivetoken.auth.js
+++ src/Components/Auth/receivetoken.auth.js
@@ -8,6 +8,8 @@ export function ReceiveToken(props) {
   urlString.split('&').map(i => i.split("=")).forEach(arr => authItem[arr[0]] = arr[1])
   if('access_token' in authItem) authItem.auth = true;
   window.localStorage.setItem('token', JSON.stringify(authItem))
+  // This is not elegant, a little hacky.  But, with no global store, and the app already loaded, I did not know of
+  // another way to get the token to the API element..
   window.location.assign('/categories')
 
   return null
